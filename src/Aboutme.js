da1de1b3836694d1338f0b293aa2639ba88361d2
jothimani2004 <jothimani88531@gmail.com>
2025-10-15 14:57:33 +0530

    change resume

--- src/Aboutme.js
+++ src/Aboutme.js
@@ -30,7 +30,7 @@ const HeroSection = () => {
 
 <div>
       <a
-        href="https://drive.google.com/file/d/10W09SZ5DmINyhQPgPDfHrTbGjzR0W507/view?usp=sharing"
+        href="https://drive.google.com/file/d/1uY_NXW3rn-jnNRgLqB2Crnmw68wFK4F3/view?usp=sharing"
         target="_blank"
       
         className="ml-14 inline-block px-6 py-3 bg-hovercolor hover:bg-color text-gray-600 font-bold rounded-lg shadow-lg hover:shadow-xl transition duration-300 ease-in-out transform hover:scale-105"
